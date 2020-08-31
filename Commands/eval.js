ad911d440738201ea823358bccd1479f5702a141
Asher Fogelson <devxml@bloxtel.tech>
2020-08-31 14:58:49 -0400

    Update eval.js

--- Commands/eval.js
+++ Commands/eval.js
@@ -1,1 +1,21 @@
-
+const { Client, MessageEmbed } = require('discord.js');
+const { prefix, owner } = require('./config.json')
+const client = new Client();
+        module.exports = { 
+            name: 'eval',
+        	  cooldown: 0,
+            description: 'Developer Command.',
+            execute(message, args){
+              if (message.content === prefix + "eval") {
+              if(message.author.id !== owner) return;
+              try {
+                const code = args.join(" ");
+                let evaled = eval(code);
+ 
+                if (typeof evaled !== "string")
+                evaled = require("util").inspect(evaled);
+ 
+                message.channel.send(clean(evaled), {code:"xl"});
+              } catch (err) {
+              message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+                
