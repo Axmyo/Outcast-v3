50ee32331326b815019ed789faa7618a80ec7729
Asher Fogelson <devxml@bloxtel.tech>
2020-10-02 09:48:04 -0400

    Update help.js
    Fixed a ton of crap

--- Commands/help.js
+++ Commands/help.js
@@ -1,17 +1,20 @@
 const { Client, MessageEmbed } = require('discord.js');
+var { prefix } = require("../OutcastAssets/config.json")
 const client = new Client();
-        module.exports = { 
-            name: 'help',
-            description: 'Learn what commands I have to offer!',
-            execute(message, args){
-            if (message.content === ';help') {
-            const embed = new MessageEmbed()
-            embed.setTitle("Bloxtel Help Menu"),
-            embed.addField("Moderation", "Kick, Ban")
-            embed.addField("Utility", "")
-            embed.addField("Animals", "Cat, Dog, Fox")     
-            embed.setColor("ff0000")
-            message.channel.send(embed)
-        }
+module.exports = { 
+    name: 'help',
+    cooldown: 5,
+    description: 'Learn what commands I have to offer!',
+    execute(message, args){
+        const embed = new MessageEmbed()
+        embed.setTitle("Outcast Help Menu"),
+        embed.addField("Prefix:", "Outcast ")
+        embed.addField("Moderation:", "Kick, Ban, purge")
+        embed.addField("Utility:", "specs, serverinfo, avatar, userinfo, userinfov2 (BETA), invite, ping, support")
+        embed.addField("Animals:", "Cat, Dog, Fox, Bunny, 666")     
+        embed.setColor("ff0000")
+        embed.setFooter("Outcast V3.1.0", "https://cdn.discordapp.com/avatars/677257480744730624/eb078b76eab8f80a6001b64cf4146fad.png?size=1024&.png")
+        embed.setTimestamp()
+        message.channel.send(embed)
     }
 };
