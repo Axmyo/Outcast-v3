009bcf759aea03bafb63c38d5700a4ffd4deb39c
Asher Fogelson <devxml@bloxtel.tech>
2020-10-17 11:10:30 -0400

    Update specs.js

--- Commands/specs.js
+++ Commands/specs.js
@@ -1,26 +1,39 @@
 const { Client, MessageEmbed } = require('discord.js');
-const { version } = require('../OutcastAssets/config.json')
+var { version, picture } = require('../OutcastAssets/config.json');
+const { execSync } = require('child_process');
+const hostname = execSync ('hostname').toString();
 const client = new Client();
-module.exports = { 
-    name: 'specs',
-    description: 'Look at info on the bot (me).',
-    cooldown: 5,
-    aliases: ['botinfo', 'bi'],
-    execute(message, args){
-        const embed = new MessageEmbed()
-        embed.setTitle("Bot Info"),
-        embed.addField("Version:", "3.1.0")
-        embed.addField("Developer:", "Asher#7028")
-        embed.addField("Version Type:", "Release")
-        embed.addField("Last Updated:", "9/21/20")
-        embed.addField("Library:", "Discord.js v12")
-        embed.addField("Current Host Platform:", "Hosted on Platinum")
-        embed.addField("Host Server Type:", "Raspberry Pi 4, 8 GB RAM")
-        embed.setColor("ff0000")
-        embed.addField("Contributors:", "Magicalbunny31#9221")
-        embed.setFooter(`Outcast ${version}`, "https://cdn.discordapp.com/avatars/677257480744730624/eb078b76eab8f80a6001b64cf4146fad.png?size=1024&.png")
-        embed.setThumbnail("https://cdn.discordapp.com/avatars/677257480744730624/eb078b76eab8f80a6001b64cf4146fad.png?size=4096&.png")
-        embed.setTimestamp()
-        message.channel.send(embed);
-    }
+        module.exports = { 
+            name: 'specs',
+            description: 'Look at info on me.',
+            cooldown: 5,
+            aliases: ['botinfo', 'bi'],
+            execute(message, args){
+                let totalSeconds = (message.client.uptime / 1000);
+                const days = Math.floor(totalSeconds / 86400);
+                totalSeconds %= 86400;
+                const hours = Math.floor(totalSeconds / 3600);
+                totalSeconds %= 3600;
+                const minutes = Math.floor(totalSeconds / 60);
+                const seconds = Math.floor(totalSeconds % 60);
+                const uptime = `${days} days, ${hours} hours, ${minutes} minutes, ${Math.round(seconds)} seconds`;
+                const embed = new MessageEmbed()
+                            
+                embed.setTitle("Bot Info"),
+                embed.setDescription("Changelog: Coming Soon!"),
+                embed.addField("Version:", `${version}`)
+                embed.addField("Developer:", "Asher#7028")
+                embed.addField("Version Type:", "Release")
+                embed.addField("Last Updated:", "9/21/20")
+                embed.addField("Library:", "Discord.js v12")
+                embed.addField("Current Host Server:", `${hostname}`)
+                embed.addField("Host Server Type:", "Raspberry Pi 4, 8 GB RAM")
+                embed.addField("Uptime:", `${uptime} `)
+                embed.setColor("ff0000")
+                embed.addField("Contributors:", `${message.client.users.cache.get('490178047325110282').tag} - Helped with a lot of command fixes.`)
+                embed.setFooter(`${version}`, `${picture}`)
+                embed.setThumbnail(`${picture}`)
+                embed.setTimestamp()
+                message.channel.send(embed)
+    }    
 };
