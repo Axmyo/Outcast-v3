0eb6de9e9910a704e2ba0ed0d3a8d6f29b656af4
Asher Fogelson <devxml@bloxtel.tech>
2020-10-17 11:12:14 -0400

    Update specs.js

--- Commands/specs.js
+++ Commands/specs.js
@@ -1,5 +1,5 @@
 const { Client, MessageEmbed } = require('discord.js');
-const { version, picture } = require('../OutcastAssets/config.json');
+const { version, picture, LastUpdated } = require('../OutcastAssets/config.json');
 const { execSync } = require('child_process');
 const hostname = execSync ('hostname').toString();
 const client = new Client();
@@ -17,22 +17,20 @@ const client = new Client();
                 const minutes = Math.floor(totalSeconds / 60);
                 const seconds = Math.floor(totalSeconds % 60);
                 const uptime = `${days} days, ${hours} hours, ${minutes} minutes, ${Math.round(seconds)} seconds`;
-                const embed = new MessageEmbed()
-                            
+                const embed = new MessageEmbed();
                 embed.setTitle("Bot Info"),
                 embed.setDescription("Changelog: Coming Soon!"),
-                embed.addField("Version:", `${version}`)
-                embed.addField("Developer:", "Asher#7028")
-                embed.addField("Version Type:", "Release")
-                embed.addField("Last Updated:", "9/21/20")
-                embed.addField("Library:", "Discord.js v12")
-                embed.addField("Current Host Server:", `${hostname}`)
-                embed.addField("Host Server Type:", "Raspberry Pi 4, 8 GB RAM")
-                embed.addField("Uptime:", `${uptime} `)
-                embed.setColor("ff0000")
-                embed.addField("Contributors:", `${message.client.users.cache.get('490178047325110282').tag} - Helped with a lot of command fixes.`)
-                embed.setFooter(`${version}`, `${picture}`)
-                embed.setThumbnail(`${picture}`)
+                embed.addField("Version:", `${version}`),
+                embed.addField("Developer:", `${message.client.users.cache.get('613527205103927367').tag} and ${message.client.users.cache.get('490178047325110282').tag}`),
+                embed.addField("Version Type:", "Alpha"),
+                embed.addField("Last Updated:", `${LastUpdated}`),
+                embed.addField("Library:", "Discord.js v12"),
+                embed.addField("Current Host Server:", `${hostname}`),
+                embed.addField("Host Server Type:", "Raspberry Pi 4, 8 GB RAM"),
+                embed.addField("Uptime:", `${uptime} `),
+                embed.setColor("ff0000"),
+                embed.setFooter(`${version}`, `${picture}`),
+                embed.setThumbnail(`${picture}`),
                 embed.setTimestamp()
                 message.channel.send(embed)
     }    
