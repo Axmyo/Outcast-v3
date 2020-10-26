a393cc0a6d8d0dd71fd9b6de0a126605bb210248
Asher Fogelson <devxml@bloxtel.tech>
2020-10-26 12:29:16 -0400

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -1,5 +1,5 @@
 const { Client, MessageEmbed } = require('discord.js');
-var { version } = require("../OutcastAssets/config.json")
+const { version, picture } = require("../OutcastAssets/config.json")
 const client = new Client();
 module.exports = {
     name: 'help',
@@ -9,12 +9,12 @@ module.exports = {
         const embed = new MessageEmbed()
         embed.setTitle("Outcast Help Menu"),
         embed.addField("Prefix:", "Outcast ")
-        embed.addField("Moderation:", "Kick, Ban, purge")
+        embed.addField("Moderation:", "ban, kick, purge")
         embed.addField("Utility:", "specs, serverinfo, avatar, userinfov1 (OUTDATED), userinfo, invite, ping")
-        embed.addField("Animals:", "Cat, Dog, Fox, Bunny, 666")  
-        embed.addField("Developer:", "Eval, Blacklist, Unblacklist")   
+        embed.addField("Animals:", "bunny, cat, dog, fox, 666")  
+        embed.addField("Developer:", "blacklist, eval, unblacklist")   
         embed.setColor("ff0000")
-        embed.setFooter(`Outcast ${version}`, "https://cdn.discordapp.com/avatars/677257480744730624/eb078b76eab8f80a6001b64cf4146fad.png?size=1024&.png")
+        embed.setFooter(`${version}`, `${picture}`)
         embed.setTimestamp()
         message.channel.send(embed)
     }
