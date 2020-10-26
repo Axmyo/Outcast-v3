3b7098aa2e80701d06532c8d9822dd6ee622122d
Asher Fogelson <devxml@bloxtel.tech>
2020-10-26 12:47:23 -0400

    Update fox.js

--- Commands/fox.js
+++ Commands/fox.js
@@ -1,18 +1,17 @@
 const { MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
 const { version } = require("../OutcastAssets/config.json");
-
 module.exports = {
-  'name':  "fox",
-  'description': "Look at random fox pics, courtesy of randomfox.ca",
-  'cooldown': '5',
-  'aliases': ['foxxo'],
-   execute(message, args){
+  name:  "fox",
+  description: "Look at random fox pics, courtesy of randomfox.ca",
+  cooldown: '5',
+  aliases: ['foxxo'],
+  execute(message, args){
       const link = "https://randomfox.ca/floof/";
       fetch(link)
       .then(res => res.json())
       .then(body => {
-        const embed = new MessageEmbed();
+          const embed = new MessageEmbed();
           embed.setColor('#FF8C00'),
           embed.setTitle('Foxxo!'),
           embed.setImage(body.image),
