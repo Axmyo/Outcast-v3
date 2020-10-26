4ee1f411a5c1f42e222a2040e344941ae8390cf3
Asher Fogelson <devxml@bloxtel.tech>
2020-10-26 12:33:44 -0400

    Fixing All Commands

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,21 +1,21 @@
 const { Client, MessageEmbed } = require('discord.js');
-const { version } = require('../OutcastAssets/config.json');
+const { version, picture } = require('../OutcastAssets/config.json');
 const client = new Client();
         module.exports = { 
         name: 'ping',
+        description: 'Check Discord\'s API Ping and my ping.',
         cooldown: 5,
         aliases: ['latency'],
-        description: 'Check Discord\'s API Ping and my ping.',
         async execute(message, args) {
         const m = await message.channel.send("Ping!");
         message.channel.bulkDelete(1, true)
-        const embed = new MessageEmbed()
+        const embed = new MessageEmbed();
         embed.setTitle("Outcast"),
         embed.addField("Bot Latency:", `${Math.round(m.createdTimestamp - message.createdTimestamp)} ms`),
         embed.addField("API Latency:", `${Math.round(message.client.ws.ping)} ms`),    
         embed.setColor("ff0000"),
         embed.setTimestamp(),
-        embed.setFooter(`Outcast ${version}`, "https://cdn.discordapp.com/avatars/677257480744730624/eb078b76eab8f80a6001b64cf4146fad.png?size=256&.png"),
+        embed.setFooter(`${version}`, `${picture}`),
         message.channel.send(embed);
         
     }
