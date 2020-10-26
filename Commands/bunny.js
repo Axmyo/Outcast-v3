4cf7948a4aa564e9a8f062becf8d103eaec87833
Asher Fogelson <devxml@bloxtel.tech>
2020-10-26 12:34:38 -0400

    Update bunny.js

--- Commands/bunny.js
+++ Commands/bunny.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 const animals = require('random-animals-api'); 
-const { version } = require("../OutcastAssets/config.json");
+const { version, picture } = require("../OutcastAssets/config.json");
 
 module.exports = {
   name:  "bunny",
@@ -9,13 +9,13 @@ module.exports = {
   aliases: ['rabbit'],
     async execute(message, args){
         await animals.bunny().then(url => {
-            const embed = new MessageEmbed()
-              embed.setColor('#FFD700')
-              embed.setTitle(`Bunny!`)
-              embed.setDescription(`**[image link](${url})**`)
-              embed.setImage(url)
-              embed.setFooter(`Outcast ${version}`, "https://cdn.discordapp.com/avatars/717033979760345198/b6efe83ce84eb7b1d63c72fbdb0de5f0.png?size=1024")
-              embed.setTimestamp()
+            const embed = new MessageEmbed();
+              embed.setColor('#FFD700'),
+              embed.setTitle(`Bunny!`),
+              embed.setDescription(`**[image link](${url})**`),
+              embed.setImage(url),
+              embed.setFooter(`${version}`, `${picture}`),
+              embed.setTimestamp(),
             return message.channel.send(embed);
             
           }).catch((error) => {
