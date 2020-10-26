2674cf078dbb98838b0966ebbd2286c67bff0546
Asher Fogelson <devxml@bloxtel.tech>
2020-10-26 12:32:27 -0400

    Update helpv2.js

--- Commands/helpv2.js
+++ Commands/helpv2.js
@@ -1,5 +1,5 @@
 const { Client, MessageEmbed } = require('discord.js');
-var { version } = require("../OutcastAssets/config.json")
+const { version, picture } = require("../OutcastAssets/config.json")
 const client = new Client();
         module.exports = { 
         name: 'helpv2',
@@ -12,7 +12,7 @@ const client = new Client();
         embed.addField("Prefix:", "Outcast ")
         embed.addField("<:banhammer:758703914643750942> Moderation", ""),
         embed.setColor("ff0000")
-        embed.setFooter(`Outcast ${version}`, "https://cdn.discordapp.com/avatars/677257480744730624/eb078b76eab8f80a6001b64cf4146fad.png?size=1024&.png")
+        embed.setFooter(`${version}`, `${picture}`)
         embed.setTimestamp()
         message.channel.send(embed);
     }
