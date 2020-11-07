c7ecc89982130580e9adcf5a953cea9992ece3f9
Ash Fogelson <devxml@bloxtel.tech>
2020-11-07 14:41:07 -0500

    Update eval.js

--- Commands/eval.js
+++ Commands/eval.js
@@ -1,34 +1,43 @@
 const { MessageEmbed } = require('discord.js');
-var { Owner, version } = require("../OutcastAssets/config.json");
+const { owner, version, picture } = require("../OutcastAssets/config.json");
 
 module.exports = { 
-          name: 'eval',
-          cooldown: 0,
-          description: 'Developer Command.',
-          execute(message, args){
-            if(!message.author.id === Owner) return(message.channel.send("Missing Permission: ``BOT_OWNER``"))
-            if(message.author.id === Owner) {
-                try {
-                  const code = args.join(" ");
-                  let evaled = eval(code);
-    
-                  if (typeof evaled !== "string")
-                  evaled = require("util").inspect(evaled);
-                  const OutputEmbed = new MessageEmbed();
-                  OutputEmbed.setTitle("Output:"),
-                  OutputEmbed.setDescription(evaled),
-                  OutputEmbed.setFooter(`Outcast ${version}`, "https://cdn.discordapp.com/avatars/677257480744730624/eb078b76eab8f80a6001b64cf4146fad.png?size=1024&.png")
-                  OutputEmbed.setTimestamp(),
-                  message.channel.send(OutputEmbed);
-        } catch (error) {
-          const ErrorEmbed = new MessageEmbed();
-          ErrorEmbed.setTitle("Error"),
-          ErrorEmbed.setDescription(`An (expected) error has occured. Error: ${error}`),
-          ErrorEmbed.setColor("ff0000"),
-          ErrorEmbed.setFooter(`Outcast ${version}`, "https://cdn.discordapp.com/avatars/677257480744730624/eb078b76eab8f80a6001b64cf4146fad.png?size=1024&.png"),
-          ErrorEmbed.setTimestamp(),
-          message.channel.send(ErrorEmbed);
-      }   
+  name: 'eval',
+  description: 'Developer Command.',
+  cooldown: 0,
+  execute(message, args){
+    if (!owner.includes(message.author.id)) { // ...
+      const PermissionEmbed = new MessageEmbed();
+      PermissionEmbed.setTitle('Error: Missing Permisssions'),
+      PermissionEmbed.addField('Error Code:', '403: Forbidden'),
+      PermissionEmbed.addField('Missing Permission:', '\`\`MANAGE_MESSAGES\`\`'),
+      PermissionEmbed.setFooter(`${version}`, `${picture}`),
+      PermissionEmbed.setTimestamp(),
+      PermissionEmbed.setColor('ff0000'),
+      message.channel.send(PermissionEmbed);
+    }
+    if (owner.includes(message.author.id)) { // ...
+      try {
+        const code = args.join(" ");
+        let evaled = eval(code);
+
+        if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+        const embed = new MessageEmbed();
+        embed.setTitle("Output:"),
+        embed.setDescription(evaled),
+        embed.setFooter(`${version}`, `${picture}`),
+        embed.setTimestamp(),
+        message.channel.send(embed);
+      } catch (error) {
+        const ErrorEmbed = new MessageEmbed();
+        ErrorEmbed.setTitle("Error!"),
+        ErrorEmbed.setDescription(`An (expected) error has occured. Error: \n${error}\n`),
+        ErrorEmbed.setFooter(`${version}`, `${picture}`),
+        ErrorEmbed.setTimestamp(),
+        message.channel.send(ErrorEmbed);
+      }
     }
   }
 };
