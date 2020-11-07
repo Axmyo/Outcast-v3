9567563f2769fe5045e141fbf66d109647f48b07
Ash Fogelson <devxml@bloxtel.tech>
2020-11-07 14:43:17 -0500

    Update blacklist.js

--- Commands/blacklist.js
+++ Commands/blacklist.js
@@ -1,19 +1,28 @@
 const { Client, MessageEmbed } = require('discord.js');
-const { owner, version } = require('../OutcastAssets/config.json');
+const { owner, version, picture } = require('../OutcastAssets/config.json');
 const db = require ('quick.db'); // ES5 module
-// const client = new Client();
+const client = new Client();
 module.exports = {
     name: "blacklist",
-    description: "OWNER ONLY!!!!! Ban users from using the bot.",
+    description: "Ban users from using the bot.",
     cooldown: 0,
     aliases: ['bl', 'botban', 'bban'],
     execute(message, args) {
-        if (message.author.id !== owner)
-            return message.channel.send("Missing Permissions: OutcastCorp Staff or Bot Owner.");
-        if (message.author.id === owner) {
-
+      if (!owner.includes(message.author.id)) { // ...				
+        const PermissionEmbed = new MessageEmbed();
+	PermissionEmbed.setTitle('Error: Missing Permisssions'),
+	PermissionEmbed.addField('Error Code:', '403: Forbidden'),
+	PermissionEmbed.addField('Missing Permission:', '\`\`BOT_OWNER\`\`'),
+	PermissionEmbed.setFooter(`${version}`, `${picture}`),
+	PermissionEmbed.setTimestamp(),
+	PermissionEmbed.setColor('ff0000'),
+        message.channel.send(PermissionEmbed);
+        return
+      };
+      if (owner.includes(message.author.id)) { // ...
         const blacklist = new db.table('blacklist'); // make a new db table
         const user = args[0]; // the provided args --MUST-- be a user's ID
+	const reason = args.slice(1).join(' ');
 
         let toBeBl;
         try {
@@ -23,18 +32,27 @@ module.exports = {
         }
         
         let isBlacklisted = blacklist.fetch (user);
-        if (isBlacklisted !== null)
-            return message.channel.send(`${message.client.users.cache.get(user).tag} is already blacklisted.`);
-              
-        blacklist.set (user, true);
+        if (isBlacklisted !== null) {
+          const ExistenentialEmbed = new MessageEmbed();
+          ExistenentialEmbed.setTitle('Error: User Already Exists In Database'),
+          ExistenentialEmbed.addField('Error Code:', '409: Resource already exists in the database.'),
+          ExistenentialEmbed.addField('Error Description:', 'The user has already been blacklisted you DUNCE.'),
+          ExistenentialEmbed.setFooter(`${version}`, `${picture}`),
+          ExistenentialEmbed.setTimestamp(),
+          ExistenentialEmbed.setColor('ff0000'),
+          message.channel.send(ExistenentialEmbed);   
+          return;
+        }       
+        blacklist.set (user, true, reason);
           
         const successembed = new MessageEmbed();
         successembed.setTitle('Success!'),
-        successembed.setDescription(`Successfully blacklisted ${message.client.users.cache.get(user).tag}.`),
-        successembed.setFooter(`Outcast ${version}`, "https://cdn.discordapp.com/avatars/677257480744730624/eb078b76eab8f80a6001b64cf4146fad.png?size=1024&.png"),
+        successembed.setDescription(`Successfully blacklisted ${message.client.users.cache.get(user).tag} for reason ${reason}`),
+        successembed.setFooter(`${version}`, `${picture}`),
         successembed.setTimestamp(),
         successembed.setColor("#00FF00"),
         message.channel.send(successembed);
-    )
-  }
+        return;
+      }
+   },
 };
