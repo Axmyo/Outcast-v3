93c36d377143f2df6799323412623070c209a5ff
Ash Fogelson <neonmadnessyt@gmail.com>
2020-11-11 09:56:12 -0500

    Update train.js

--- commands/train.js
+++ commands/train.js
@@ -1,19 +1,18 @@
 const { MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
 const{ version, picture } = require("../OutcastAssets/config.json");
-
 module.exports = {
-  'name':  "train",
-  'description': "Look at random train pics",
-  'cooldown': '5',
-  'aliases': ['trainpic'],
+  name:  "train",
+  description: "Look at random train pics",
+  cooldown: '5',
+  aliases: ['trainpic'],
   execute(message, args){
       const link = "https://api.outcastcorp.us/api/";
       fetch(link)
         .then(res => res.json())
         .then(body => {
             const TrainEmbed = new MessageEmbed();
-            TrainEmbed.setColor('#0000000'),
+            TrainEmbed.setColor('#000000'),
             TrainEmbed.setTitle('Train!'),
             TrainEmbed.setImage(body.image),
             TrainEmbed.setFooter(`${version}`, `${picture}`),
