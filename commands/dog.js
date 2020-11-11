39ec3d8601af0d7dbfbead47b22cbffb1b3ab971
Ash Fogelson <neonmadnessyt@gmail.com>
2020-11-11 10:06:07 -0500

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -1,25 +1,23 @@
 const { MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
 const { version, picture } = require("../OutcastAssets/config.json");
-
 module.exports = {
-  'name':  "dog",
-  'description': "Look at random dog pics, courtesy of dog.ceo",
-  'cooldown': '5',
-  'aliases': ['doggo', 'puppy'],
+  name:  "dog",
+  description: "Look at random dog pics, courtesy of dog.ceo",
+  cooldown: '5',
+  aliases: ['doggo', 'puppy'],
   execute(message, args){
       const link = "https://dog.ceo/api/breeds/image/random";
       fetch(link)
         .then(res => res.json())
         .then(body => {
-          const embed = new MessageEmbed();
-            embed.setColor('#FF8C00'),
-            embed.setTitle('Doggo!'),
-            embed.setImage(body.message),
-            embed.setFooter(`${version}`, `${picture}`),
-            embed.setTimestamp(),
-
-          message.channel.send(embed);
+          const DogEmbed = new MessageEmbed();
+          DogEmbed.setColor('#FF8C00'),
+          DogEmbed.setTitle('Doggo!'),
+          DogEmbed.setImage(body.message),
+          DogEmbed.setFooter(`${version}`, `${picture}`),
+          DogEmbed.setTimestamp(),
+          message.channel.send(DogEmbed);
       });
   },
 };
