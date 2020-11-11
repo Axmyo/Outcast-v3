09b00a90ec3071c562c03ae94e1e8eb9cedc73f3
Ash Fogelson <neonmadnessyt@gmail.com>
2020-11-11 10:21:46 -0500

    Update 666.js

--- commands/666.js
+++ commands/666.js
@@ -1,18 +1,20 @@
-const { Client, MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+const { picture } = require('../OutcastAssets/config.json');
 module.exports = {
     name: '666',
-    cooldown: 5,
     description: 'Learn what commands I have to offer!',
+    cooldown: 5,
+    aliases: ['wewilltakeover'],
     execute(message, args){
-        const embed = new MessageEmbed();
-        embed.setTitle("666"),
-        embed.addField("666", "666"),
-        embed.addField("666", "666"),
-        embed.addField("666", "666"),
-        embed.addField("666", "666"),
-        embed.setColor("#ff0000"),
-        embed.setFooter("666", "https://cdn.discordapp.com/avatars/717033979760345198/b6efe83ce84eb7b1d63c72fbdb0de5f0.png?size=1024"),
-        embed.setTimestamp(),
-        message.channel.send(embed);
+        const 666Embed = new MessageEmbed();
+        666Embed.setTitle("666"),
+        666Embed.addField("666", "666"),
+        666Embed.addField("666", "666"),
+        666Embed.addField("666", "666"),
+        666Embed.addField("666", "666"),
+        666Embed.setColor("#ff0000"),
+        666Embed.setFooter("666", `${picture}`),
+        666Embed.setTimestamp(),
+        message.channel.send(666Embed);
     }
 };
