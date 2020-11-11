c9ccede9bb86dfddcfd769a82fc1375b24d58696
Ash Fogelson <neonmadnessyt@gmail.com>
2020-11-11 10:27:51 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,15 +6,15 @@ module.exports = {
         aliases: ['latency'],
         description: 'Check Discord\'s API Ping and my ping.',
         async execute(message, args) {
-        const m = await message.channel.send("Ping!");
-        message.channel.bulkDelete(1, true)
-        const PingEmbed = new MessageEmbed()
-        PingEmbed.setTitle("Outcast"),
-        PingEmbed.addField("Bot Latency:", `${Math.round(m.createdTimestamp - message.createdTimestamp)} ms`),
-        PingEmbed.addField("API Latency:", `${Math.round(message.client.ws.ping)} ms`),    
-        PingEmbed.setColor("ff0000"),
-        PingEmbed.setTimestamp(),
-        PingEmbed.setFooter(`${version}`, `${picture}`),
-        message.channel.send(PingEmbed);
+                const m = await message.channel.send("Ping!");
+                message.channel.bulkDelete(1, true)
+                const PingEmbed = new MessageEmbed();
+                PingEmbed.setTitle("Outcast"),
+                PingEmbed.addField("Bot Latency:", `${Math.round(m.createdTimestamp - message.createdTimestamp)} ms`),
+                PingEmbed.addField("API Latency:", `${Math.round(message.client.ws.ping)} ms`),    
+                PingEmbed.setColor("ff0000"),
+                PingEmbed.setTimestamp(),
+                PingEmbed.setFooter(`${version}`, `${picture}`),
+                message.channel.send(PingEmbed);
     }
 };
