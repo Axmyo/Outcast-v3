22a5926d688e7d1ad1261412448f583733c496f2
Ash Fogelson <neonmadnessyt@gmail.com>
2020-11-11 10:16:49 -0500

    Update fox.js

--- commands/fox.js
+++ commands/fox.js
@@ -1,25 +1,23 @@
 const { MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
 const{ version, picture } = require("../OutcastAssets/config.json");
-
 module.exports = {
-  'name':  "fox",
-  'description': "Look at random fox pics, courtesy of randomfox.ca",
-  'cooldown': '5',
-  'aliases': ['foxxo'],
+  name:  "fox",
+  description: "Look at random fox pics, courtesy of randomfox.ca",
+  cooldown: '5',
+  aliases: ['foxxo'],
   execute(message, args){
       const link = "https://api.outcastcorp.us/fox/floof/";
       fetch(link)
-        .then(res => res.json())
-        .then(body => {
-          const embed = new MessageEmbed()
-            embed.setColor('#FF8C00'),
-            embed.setTitle('Foxxo!'),
-            embed.setImage(body.image),
-            embed.setFooter(`${version}`, `${picture}`),
-            embed.setTimestamp(),
-
-          message.channel.send(embed);
+      .then(res => res.json())
+      .then(body => {
+        const FoxEmbed = new MessageEmbed();
+        FoxEmbed.setColor('#FF8C00'),
+        FoxEmbed.setTitle('Foxxo!'),
+        FoxEmbed.setImage(body.image),
+        FoxEmbed.setFooter(`${version}`, `${picture}`),
+        FoxEmbed.setTimestamp(),
+        message.channel.send(FoxEmbed);
       });
   },
 };
