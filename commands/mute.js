7a183607b27703255058aa8c73c9269c99e2d7a5
Ash Fogelson <neonmadnessyt@gmail.com>
2020-11-25 12:14:47 -0500

    Create mute.js

---
+++ commands/mute.js
@@ -0,0 +1,12 @@
+const { MessageEmbed } = require("discord.js");
+
+
+module.exports = {
+  name: "",
+  description: "",
+  cooldown: 5,
+  aliases: ["tape"],
+  execute(message, args) {
+    
+  }
+}
