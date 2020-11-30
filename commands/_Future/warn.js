7fe3761764bbbb39f879325da34787daf305f0c7
Ash Fogelson <neonmadnessyt@gmail.com>
2020-11-30 12:27:13 -0500

    Create warn.js

---
+++ commands/_Future/warn.js
@@ -0,0 +1,11 @@
+const { MessageEmbed } = require("discord.js");
+const db = require("quick.db");
+module.exports = {
+  name: "warn",
+  description: "Warn members.",
+  cooldown: 5,
+  aliases: ["strike", ""],
+  execute(message, args) {
+    const reason = args
+  }
+};
