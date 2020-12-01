1915dcbd9b45dc11681da0076e7ce676039b5e08
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-01 11:04:36 -0500

    Create goat.js

---
+++ commands/goat.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+const { version, picture } = require("../OutcastAssets/config.json");
+module.exports = {
+  name:  "goat",
+  description: "Look at random goat pics, courtesy of placegoat.com.",
+  cooldown: '5',
+  aliases: [],
+  execute(message, args){
+    const link = "https://placegoat.com/200/200";
+    fetch(link)
+    .then(res => res.json())
+    .then(body => {
+        const GoatEmbed = new MessageEmbed()
+        .setColor("#ffffff"),
+        .setTitle("Goat!"),
+        .setImage(body.file),
+        .setFooter(`${version}`, `${picture}`),
+        .setTimestamp(),
+        message.channel.send(GoatEmbed);
+    });
+  },
+};
