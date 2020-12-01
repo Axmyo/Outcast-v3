a939a44adbacf2dae280017b3a36bcebda557e41
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-01 10:25:33 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -5,77 +5,77 @@ module.exports = {
 	description: "Ban's a member",
 	cooldown: 0,
 	aliases: ['boot'],
-	execute(message, args){
-		if (!message.member.hasPermission("BAN_MEMBERS")) {
-			const PermissionEmbed = new MessageEmbed();
-			PermissionEmbed.setTitle('Error: Missing Permisssions'),
-			PermissionEmbed.addField('Error Code:', '403: Forbidden'),
-			PermissionEmbed.addField('Missing Permission:', '\`\`BAN_MEMBERS\`\`, \`\`ADMINISTRATOR\`\`, or Server Owner'),
-			PermissionEmbed.setFooter(`${version}`, `${picture}`),
-			PermissionEmbed.setTimestamp(),
-			PermissionEmbed.setColor('ff0000'),
-			message.channel.send(PermissionEmbed);
-		}
-		if (message.member.hasPermission("BAN_MEMBERS")) {
-			const user = message.mentions.users.first();
-			if (user) {
-				const reason = args.slice(1).join(' ');
-				const member = message.guild.member(user);
-				// If the member is in the guild
-				if (member) {
-					member
-						const BannedEmbed = new MessageEmbed();
-						BannedEmbed.setTitle("Outcast Moderation"),
-						BannedEmbed.setDescription(`You have been banned from ${message.guild.name}.`),
-						BannedEmbed.addField("Reason:", `${reason}`),
-						BannedEmbed.addField("Moderator:", `${message.author.tag}`),
-						BannedEmbed.setFooter(`${version}`, `${picture}`),
-						BannedEmbed.setTimestamp(),
-						BannedEmbed.setColor("#ff0000"),
-						member.send(BannedEmbed);  
-						member.ban({reason: `${message.author.tag}:${reason}`})
-						.then(() => {
-						const SuccessEmbed = new MessageEmbed()
-						SuccessEmbed.setTitle("Success!"),
-						SuccessEmbed.setDescription(`Successfully banned ${member}: ${reason}.`),
-						SuccessEmbed.setFooter(`${version}`, `${picture}`),
-						SuccessEmbed.setTimestamp(),
-						SuccessEmbed.setColor('00FF00'),
-						message.channel.send(SuccessEmbed);
-					})
-					.catch(err => {
-						const ErrorEmbed = new MessageEmbed();
-						ErrorEmbed.setTitle("Error."),
-						ErrorEmbed.setDescription("An unexpected error has occcured."),
-						ErrorEmbed.addField("Error (Give to my support team please):", `${err}`),	
-						ErrorEmbed.setFooter(`${version}`, `${picture}`),	
-						ErrorEmbed.setTimestamp(),
-						message.channel.send(ErrorEmbed);
-						console.error(`${guild.id}: ${err}`);
-					});
-					} else {
-						const Error2 = new MessageEmbed();
-						Error2.setTitle('Error: User Not Found'),
-						Error2.addField('Error Code:', '404: Not Found'),							
-						Error2.setFooter(`${version}`, `${picture}`),
-						Error2.setTimestamp(),
-						Error2.setColor('ff0000'),
-						message.channel.send(Error2);
-					}
-					} else {
-						// Otherwise, if no user was mentioned
-						const Error3 = new MessageEmbed();
-						Error3.setTitle('Error: No User Mentioned'),
-						Error3.addField('Error Code:', '1000'),
-						Error3.addField('Error Description:', 'You didn\'t mention a user to kick.'),
-						Error3.setFooter(`${version}`, `${picture}`),
-						Error3.setTimestamp(),
-						Error3.setColor('ff0000'),
-						message.channel.send(Error3);
-			}
-			if (reason === null) {
-				let reason === "No Reason Provided."
-			}
-		}
+	execute(message, args) {
+	    const member = message.mentions.members.first();
+
+    const PermissionEmbed = new MessageEmbed()
+	.setTitle('Error: Missing Permisssions')
+	.addField('Error Code:', '403: Forbidden')
+	.addField('Missing Permission:', '\`\`BAN_MEMBERS\`\`, \`\`ADMINISTRATOR\`\`, or Server Owner')
+	.setFooter(version, picture)
+	.setTimestamp()
+      	.setColor('ff0000')
+      
+    if (!message.member.hasPermission("BAN_MEMBERS"))
+      return message.channel.send(PermissionEmbed);
+
+
+    
+    const noMentionedUser = new MessageEmbed()
+      .setTitle('Error: No User Mentioned')
+      .addField('Error Code:', '1000')
+      .addField('Error Description:', 'You didn\'t mention a user to kick.')
+      .setFooter(`${version}`, `${picture}`)
+      .setTimestamp()
+      .setColor('ff0000')
+
+    if (!member)
+      return message.channel.send(noMentionedUser);
+
+
+
+    const cannotBanUser = new MessageEmbed()
+      .setTitle('Error: Cannot Ban User')
+      .addField('Error Code:', '')
+      .addField('Error Description:', '')
+      .setFooter(`${version}`, `${picture}`)
+      .setTimestamp()
+      .setColor('ff0000')
+
+    if (!member.bannable)
+      return message.channel.send(cannotBanUser);
+
+
+
+    let reason = args.slice(1).join(" ");
+    if (!reason) reason = "No Reason Provided.";
+
+
+
+    const bannedEmbed = new MessageEmbed()
+			.setTitle("Outcast Moderation")
+			.setDescription(`You have been banned from ${message.guild.name}.`)
+			.addField("Reason:", `${reason1}`)
+			.addField("Moderator:", `${message.author.tag}`)
+			.setFooter(`${version}`, `${picture}`)
+			.setTimestamp()
+      .setColor("#ff0000")
+
+    const successEmbed = new MessageEmbed()
+      .setTitle("Success!")
+      .setDescription(`Successfully banned ${member}: ${reason}.`)
+      .setFooter(`${version}`, `${picture}`)
+      .setTimestamp()
+      .setColor('00FF00')
+            
+    return member.send(bannedEmbed)
+      .then(() => {
+        member.ban({reason: `${message.author.tag}:${reason}`});
+        message.channel.send(successEmbed);
+      })
+      .catch(() => {
+        member.ban({reason: `${message.author.tag}:${reason}`});
+        message.channel.send(successEmbed);
+    		});
 	}
 };
