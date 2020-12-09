7e1d80260cc5a8c702973f173c125052cc8c7ed8
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-09 08:21:57 -0500

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -4,7 +4,7 @@ const { version, picture } = require("../OutcastAssets/config.json");
 module.exports = {
   name:  "cat",
   description: "Look at random cat pics, courtesy of random.cat/meow",
-  cooldown: '5',
+  cooldown: 5,
   aliases: ['kitty', 'kitten'],
   execute(message, args){
       const link = "https://aws.random.cat/meow";
