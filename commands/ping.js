3ae2b494d7bc46e9cd4604e08ce7f6c7b1eb3441
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-09 17:59:38 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,20 +1,20 @@
-const { MessageEmbed } = require('discord.js');
-const { version, picture } = require('../OutcastAssets/config.json');
+const { MessageEmbed } = require("discord.js");
+const { version, picture } = require("../OutcastAssets/config.json");
 module.exports = { 
         name: 'ping',
+        description: 'Check Discord\'s API Ping and my ping.',
         cooldown: 5,
         aliases: ['latency'],
-        description: 'Check Discord\'s API Ping and my ping.',
         async execute(message, args) {
                 const m = await message.channel.send("Ping!");
                 message.channel.bulkDelete(1, true)
-                const PingEmbed = new MessageEmbed();
-                PingEmbed.setTitle("Outcast"),
-                PingEmbed.addField("Bot Latency:", `${Math.round(m.createdTimestamp - message.createdTimestamp)} ms`),
-                PingEmbed.addField("API Latency:", `${Math.round(message.client.ws.ping)} ms`),    
-                PingEmbed.setColor("ff0000"),
-                PingEmbed.setTimestamp(),
-                PingEmbed.setFooter(`${version}`, `${picture}`),
+                const PingEmbed = new MessageEmbed()
+                .setTitle("Outcast")
+                .addField("Bot Latency:", `${Math.round(m.createdTimestamp - message.createdTimestamp)} ms`)
+                .addField("API Latency:", `${Math.round(message.client.ws.ping)} ms`)
+                .setColor("#FF0000")
+                .setTimestamp()
+                PingEmbed.setFooter(version, picture)
                 message.channel.send(PingEmbed);
     }
 };
