d411956139e35b21f8ae21df1f1d8b08d87748f7
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-09 08:33:25 -0500

    Minor Effieciency Improvements

--- commands/fox.js
+++ commands/fox.js
@@ -1,22 +1,22 @@
-const { MessageEmbed } = require('discord.js');
-const fetch = require('node-fetch');
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
 const{ version, picture } = require("../OutcastAssets/config.json");
 module.exports = {
   name:  "fox",
   description: "Look at random fox pics, courtesy of randomfox.ca",
-  cooldown: '5',
+  cooldown: 5,
   aliases: ['foxxo'],
   execute(message, args){
       const link = "https://api.outcastcorp.us/fox/floof/";
       fetch(link)
       .then(res => res.json())
       .then(body => {
-        const FoxEmbed = new MessageEmbed();
-        FoxEmbed.setColor('#FF8C00'),
-        FoxEmbed.setTitle('Foxxo!'),
-        FoxEmbed.setImage(body.image),
-        FoxEmbed.setFooter(`${version}`, `${picture}`),
-        FoxEmbed.setTimestamp(),
+        const FoxEmbed = new MessageEmbed()
+        .setColor("#FF8C00")
+        .setTitle("Foxxo!")
+        .setImage(body.image)
+        .setFooter(version, picture)
+        FoxEmbed.setTimestamp()
         message.channel.send(FoxEmbed);
       });
   },
