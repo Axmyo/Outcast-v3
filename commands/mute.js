fc595a0cc91012c7b09c0c39880816b867d9fc7d
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-09 08:54:14 -0500

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -2,8 +2,8 @@ const { MessageEmbed } = require("discord.js");
 
 
 module.exports = {
-  name: "",
-  description: "",
+  name: "mute",
+  description: "Mute people from sending mesages on your server.",
   cooldown: 5,
   aliases: ["tape"],
   execute(message, args) {
