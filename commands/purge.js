1511183be7a0c5f661daac59b0440163f52a13ba
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-10 08:10:17 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,18 +1,18 @@
-const { MessageEmbed } = require('discord.js');
-const { version, picture } = require('../OutcastAssets/config.json');
+const { MessageEmbed } = require("discord.js");
+const { version, picture } = require("../OutcastAssets/config.json");
 module.exports = {
 	name: 'purge',
 	description: 'Purge up to 99 messages.',
 	execute(message, args) {
 		if(!message.member.hasPermission('MANAGE_MESSAGES')) {
-			const PermissionEmbed = new MessageEmbed();
-			PermissionEmbed.setTitle('Error: Missing Permisssions'),
-			PermissionEmbed.addField('Error Code:', '503: Forbidden'),
-			PermissionEmbed.addField('Missing Permission:', '\`\`MANAGE_MESSAGES\`\`'),
-			PermissionEmbed.setFooter(`${version}`, `${picture}`),
-			PermissionEmbed.setTimestamp(),
-			PermissionEmbed.setColor('ff0000'),
-			message.channel.send(PermissionEmbed);
+			const PermissionEmbed = new MessageEmbed()
+			.setTitle("Error: Missing Permisssions")
+			.addField('Error Code:', '503: Forbidden')
+			.addField('Missing Permission:', '\`\`MANAGE_MESSAGES\`\`')
+			.setFooter(version, picture)
+			.setTimestamp()
+			.setColor("#FF0000")
+			return message.channel.send(PermissionEmbed);
 		};
 		if(message.member.hasPermission('MANAGE_MESSAGES')) {
 		const amount = parseInt(args[0]) + 1;
@@ -22,17 +22,17 @@ module.exports = {
 		} else if (amount <= 1) {
 			return message.reply('you need to input a number over 1.');
 		}
-
+			
 		message.channel.bulkDelete(amount, true).catch(err => {
 			console.error(err);
-			const ErrorEmbed = new MessageEmbed();
-			ErrorEmbed.setTitle("There was an error purging messages in this channel."),
-			ErrorEmbed.setDescription(`While attempting to purge messages, an unknown error has occured. Please give this error to my support team: \n ${err}`),
-			ErrorEmbed.setFooter(`${version}`, `${picture}`),
-			ErrorEmbed.setTimestamp(),
-			ErrorEmbed.setColor('ff0000'),
-			message.channel.send(ErrorEmbed);
+			const ErrorEmbed = new MessageEmbed()
+			.setTitle("There was an error purging messages in this channel.")
+			.setDescription(`While attempting to purge messages, an unknown error has occured. Please give this error to my support team: \n ${err}`)
+			.setFooter(version, picture)
+			.setTimestamp()
+			.setColor("#FF0000")
+			return message.channel.send(ErrorEmbed);
 		});
-}
-},
+		}
+	},
 };
