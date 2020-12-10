fa425889d64b8d62e9137810871f75d283160247
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-10 08:26:02 -0500

    Update specs.js

--- commands/specs.js
+++ commands/specs.js
@@ -1,24 +1,24 @@
-const { Client, MessageEmbed } = require('discord.js');
-const { version, picture, LastUpdated, changelog, vtype } = require('../OutcastAssets/config.json');
-const { execSync } = require('child_process');
-const si = require('systeminformation');
+const { Client, MessageEmbed } = require("discord.js");
+const { version, picture, LastUpdated, changelog, vtype } = require("../OutcastAssets/config.json");
+const { execSync } = require("child_process");
+const si = require("systeminformation");
 const hostname = execSync ('hostname').toString();
 const client = new Client();
 module.exports = {
-    name: 'specs',
-    description: 'Look at info on me.',
-    cooldown: 5,
-    aliases: ['botinfo', 'bi'],
-    async execute(message, args){
+	name: "specs",
+	description: "Look at info on me.",
+	cooldown: 5,
+	aliases: ['botinfo', 'bi'],
+	async execute(message, args){
 		let totalSeconds = (message.client.uptime / 1000);
 		const days = Math.floor(totalSeconds / 86400);
 		totalSeconds %= 86400;
 		const hours = Math.floor(totalSeconds / 3600);
-        totalSeconds %= 3600;
-        const minutes = Math.floor(totalSeconds / 60);
-        const seconds = Math.floor(totalSeconds % 60);
-        const uptime = `${days} days, ${hours} hours, ${minutes} minutes, ${Math.round(seconds)} seconds`;
-        const cpu = await si.cpu();
+		totalSeconds %= 3600;
+		const minutes = Math.floor(totalSeconds / 60);
+		const seconds = Math.floor(totalSeconds % 60);
+		const uptime = `${days} days, ${hours} hours, ${minutes} minutes, ${Math.round(seconds)} seconds`;
+		const cpu = await si.cpu();
 		const hostspecs = `${cpu.processors > 1 ? `(${cpu.processors}x) ` : ''}` + `${cpu.manufacturer} ${cpu.brand} @ ${cpu.speed}GHz\n    ${cpu.processors} processor${cpu.processors > 1 ? 's' : ''}, ${cpu.cores} core${cpu.cores > 1 ? 's' : ''}`;
 		const mem = await si.mem();
 		const memKB = (mem.total / 1024) + ' KB';
@@ -27,24 +27,24 @@ module.exports = {
 		const memoryUsage = process.memoryUsage();
 		const heapUsedMB = Math.round(memoryUsage.heapUsed / (1024 ** 2));
 		const heapTotalMB = Math.round(memoryUsage.heapTotal / (1024 ** 2));
-		const SpecsEmbed = new MessageEmbed();
-		SpecsEmbed.setTitle("Bot Info"),
-        SpecsEmbed.setDescription(`**Changelog:** \n \`\`${changelog}\`\``),
-        SpecsEmbed.addField("Version:", `${version}`, true),
-        SpecsEmbed.addField("Version Type:", `${vtype}`, true),
-        SpecsEmbed.addField("Library:", "Discord.js v12", true),
-        SpecsEmbed.addField("Last Updated:", `${LastUpdated}`, true),
-        SpecsEmbed.addField("Developers:", `${message.client.users.cache.get('613527205103927367').tag} and ${message.client.users.cache.get('490178047325110282').tag}`, true),
-        SpecsEmbed.addField("Current Host Server:", `${hostname}`, true),
-		SpecsEmbed.addField("Total RAM Availible:", `${memGB}`, true),
-		SpecsEmbed.addField("Usable RAM:", `test`, true),
-		SpecsEmbed.addField("Total RAM In Use:", ` gb, mb, kb`, true),
-        SpecsEmbed.addField("Host Server Specs:", `${hostspecs}`, true),
-        SpecsEmbed.addField("Uptime:", `${uptime} `, true),
-		SpecsEmbed.addField("Guilds:", `${client.guilds.cache.size}`, true)
-        SpecsEmbed.setColor("ff0000"),
-        SpecsEmbed.setFooter(`${version}`, `${picture}`),
-        SpecsEmbed.setTimestamp(),
-        message.channel.send(SpecsEmbed);
+		const SpecsEmbed = new MessageEmbed()
+		.setTitle("Bot Info")
+		.setDescription(`**Changelog:** \n \`\`${changelog}\`\``)
+		.addField("Version:", `${version}`, true)
+		.addField("Version Type:", `${vtype}`, true)
+		.addField("Library:", "Discord.js v12", true)
+		.addField("Last Updated:", `${LastUpdated}`, true)
+		.addField("Developers:", `${message.client.users.cache.get('613527205103927367').tag} and ${message.client.users.cache.get('490178047325110282').tag}`, true)
+		.addField("Current Host Server:", `${hostname}`, true)
+		.addField("Total RAM Availible:", `${memGB}`, true)
+		.addField("Usable RAM:", `test`, true)
+		.addField("Total RAM In Use:", ` gb, mb, kb`, true)
+		.addField("Host Server Specs:", `${hostspecs}`, true)
+		.addField("Uptime:", `${uptime} `, true)
+		.addField("Guilds:", `${client.guilds.cache.size}`, true)
+		.setColor("#FF0000")
+		.setFooter(version, picture)
+		.setTimestamp()
+		message.channel.send(SpecsEmbed);
 	}    
 };
