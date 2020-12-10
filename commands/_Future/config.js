dd96f8203f17cc82459a45a4ac9b727b93efab22
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-10 09:26:05 -0500

    Create config.js

---
+++ commands/_Future/config.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require("discord.js");
+const { version, picture } = require("../OutcastAssets/config.json");
+module.exports = {
+	name: "config",
+	description: "Configure Outcast for your server.",
+	cooldown: 0,
+	aliases: [],
+	execute(message, args) {
+		let setting = args[0];
+		let unrequiredstuff = args[1];
+		if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("No.");
+		if (message.member.hasPermission("ADMINISTRATOR")) {    
+			if (!setting) {
+    				return message.channel.send("Error: No Setting Suplied"); 
+    			},
+			if (setting) {
+				if (setting === "muterole") {
+					const MuteRolePromt = new MessageEmbed()
+						.setTitle("Success!")
+						.setDescription(`Successfully created and configured new mute role:\nName: ${name}\nColor: ${color}\n`)
+						.setFooter(version, picture)
+						.setTimestamp()
+						.setColor("#ff0000")
+						message.channel.send(MuteRolePrompt)
+					guild.roles.create({name: name, color: color, permissions: 
+				},
