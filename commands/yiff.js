ecc759a53f58c0807c5e4314cd9193104f385249
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-10 08:52:13 -0500

    Update yiff.js

--- commands/yiff.js
+++ commands/yiff.js
@@ -8,44 +8,42 @@ module.exports = {
 	aliases: ['furryporn'],
 	async execute(message, args) {
 		if(!message.channel.nsfw) {
-			const NotNSFWEmbed = new MessageEmbed();
-			NotNSFWEmbed.setTitle("Error: Channel Is Not NSFW"),
-			NotNSFWEmbed.addField("Error Code:", "403: Forbidden"),
-			NotNSFWEmbed.addField("Error Description:", "You cannot run NSFW commands in non-NSFW channels."),
-			NotNSFWEmbed.setFooter(`${version}`, `${picture}`),
-			NotNSFWEmbed.setTimestamp(),
-			NotNSFWEmbed.setColor('ff0000'),
-			message.channel.send(NotNSFWEmbed);
-			return;
+			const NotNSFWEmbed = new MessageEmbed()
+			.setTitle("Error: Channel Is Not NSFW")
+			.addField("Error Code:", "403: Forbidden")
+			.addField("Error Description:", "You cannot run NSFW commands in non-NSFW channels.")
+			.setFooter(version, picture)
+			.setTimestamp()
+			.setColor("#FF0000")
+			return message.channel.send(NotNSFWEmbed);
 		};
 		if(message.channel.nsfw) {
 			let required = args[0];
 			let yiff = require("yiff");
 			let config = { 
-			  creator: "Ash_#7028", 
+			  creator: "Ash.#7028", 
 			  name: "Outcast",
-			  version: "Outcast v3.2.0"
+			  version: "Outcast v3.2.1.1"
 			};
 			if (!required) {
-				const ErrorEmbed = new MessageEmbed();
-				ErrorEmbed.setTitle("Error"),
-				ErrorEmbed.setDescription("You must define tags eg ``gay``, ``lesbian``, or ``straight``. Keep in mind though that these are not the only tags!"),
-				ErrorEmbed.addField("Error Code:", `404: Not Found`),
-				ErrorEmbed.setFooter(`${version}`, `${picture}`),
-				ErrorEmbed.setTimestamp(),
-				ErrorEmbed.setColor('ff0000'),
-				message.channel.send(ErrorEmbed);
-				return;
+				const ErrorEmbed = new MessageEmbed()
+				.setTitle("Error")
+				.setDescription("You must define tags eg ``gay``, ``lesbian``, or ``straight``. Keep in mind though that these are not the only tags!")
+				.addField("Error Code:", "404: Not Found")
+				.setFooter(version, picture)
+				.setTimestamp()
+				.setColor("#FF0000")
+				return message.channel.send(ErrorEmbed);
 			};
 			let e6 = new yiff.e621(config);
 			await e6.request(`${required}`).then(res => {
-				const embed = new MessageEmbed();
-				embed.setColor('ff0000'),
-				embed.setTitle(`Yiff`),
-				embed.setDescription(`**Image Link:** \n ${res.page}`),
-				embed.setImage(res.image),
-				embed.setFooter(`${version}`, `${picture}`),
-				embed.setTimestamp(),
+				const embed = new MessageEmbed()
+				.setColor("#FF0000")
+				.setTitle("Yiff")
+				.setDescription(`**Image Link:** \n ${res.page}`)
+				.setImage(res.image)
+				.setFooter(version, picture)
+				.setTimestamp()
 				message.channel.send(embed);
 			});
 		}
