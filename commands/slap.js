03bacf09451cf6823b6422c953931b920325bab4
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-10 08:18:52 -0500

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -1,19 +1,17 @@
-const { MessageEmbed } = require('discord.js');
-const { version, picture } = require('../OutcastAssets/config.json');
-
+const { MessageEmbed } = require("discord.js");
+const { version, picture } = require("../OutcastAssets/config.json");
 module.exports = {
-	name: 'slap',
-	description: 'Ever wanted to slap one of your friends virtually? Well now you can.',
+	name: "slap",
+	description: "Ever wanted to slap one of your friends virtually? Well now you can.",
 	cooldown: 5,
 	aliases: ['smack', 'whap'],
 	execute(message, args) {
 		const PersonToSlap = message.mentions.members.first();
-		const SlapEmbed = new MessageEmbed();
-		SlapEmbed.setAuthor(message.author.tag, message.author.displayAvatarURL()),
-		SlapEmbed.set
-		SlapEmbed.setDescription(`${message.author} slaps ${PersonToSlap} excruciatingly hard. Ouch!`),
-		SlapEmbed.setFooter(`${version}`, `${picture}`),
-		SlapEmbed.setTimestamp(),
+		const SlapEmbed = new MessageEmbed()
+		.setAuthor(message.author.tag, message.author.displayAvatarURL())
+		.setDescription(`${message.author} slaps ${PersonToSlap} excruciatingly hard. Ouch!`)
+		.setFooter(version, picture)
+		.setTimestamp()
 		message.channel.send(SlapEmbed);
 	}
 };
