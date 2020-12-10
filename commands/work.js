cbac7f4a0c52f127cba15f116be1fb81e31cc00a
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-10 08:47:17 -0500

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -1,11 +1,11 @@
-const { MessageEmbed, Guild } = require('discord.js');
-const db = require('quick.db');
+const { MessageEmbed, Guild } = require("discord.js");
+const db = require("quick.db");
 
 module.exports = {
     name: "work",
     description: "Work for money with this command.",
     cooldown: 5,
-    aliases: ['getfuckingmoneybitchelsegetstabbed'],
+    aliases: [''],
     execute(message, args) {
         const GuildID = message.guild.id;
         const guildID = new db.table(GuildID);
