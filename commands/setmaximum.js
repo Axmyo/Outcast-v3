8261f37032682e05b2804bccc5a8f265c6ef6bac
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-10 08:16:41 -0500

    Update setmaximum.js

--- commands/setmaximum.js
+++ commands/setmaximum.js
@@ -1,6 +1,6 @@
-const { MessageEmbed } = require('discord.js');
-const db = require('quick.db');
-const { version, picture } = require('../OutcastAssets/config.json')
+const { MessageEmbed } = require("discord.js");
+const db = require("quick.db");
+const { version, picture } = require("../OutcastAssets/config.json");
 
 module.exports = {
     name: "setmaximum",
@@ -9,26 +9,25 @@ module.exports = {
     aliases: ['setmax', 'set-maximum', 'set-max'],
     execute(message, args) {
         if (!message.member.hasPermission('ADMINISTRATOR')) {
-            const PermissionEmbed = new MessageEmbed();
-            PermissionEmbed.setTitle('Error: Missing Permisssions'),
-            PermissionEmbed.addField('Error Code:', '403: Forbidden'),
-            PermissionEmbed.addField('Missing Permission:', '\`\`ADMINISTRATOR\`\`or Server Owner'),
-            PermissionEmbed.setFooter(`${version}`, `${picture}`),
-            PermissionEmbed.setTimestamp(),
-            PermissionEmbed.setColor('ff0000'),
-            message.channel.send(PermissionEmbed);
+            const PermissionEmbed = new MessageEmbed()
+            .setTitle('Error: Missing Permisssions')
+            .addField('Error Code:', '403: Forbidden')
+            .addField('Missing Permission:', '\`\`ADMINISTRATOR\`\`or Server Owner')
+            .setFooter(version, picture)
+            .setTimestamp()
+            .setColor("#FF0000")
+            return message.channel.send(PermissionEmbed);
         };
         if (message.member.hasPermission('ADMINISTRATOR')) {
-            console.log("Runs bitch.")
             const maximum = args[0];
             const GuildID = new db.table('economy');
             GuildID.set(`Guild ID: ${message.guild.id}.maximum`, maximum);
-            const SuccessEmbed = new MessageEmbed();
-            SuccessEmbed.setTitle("Success!"),
-            SuccessEmbed.setDescription(`Successfully set the maximum for this guild to ${maximum}.`),
-            SuccessEmbed.setFooter(`${version}`, `${picture}`),
-            SuccessEmbed.setTimestamp(),
-            SuccessEmbed.setColor('#00FF00'),
+            const SuccessEmbed = new MessageEmbed()
+            .setTitle("Success!")
+            .setDescription(`Successfully set the maximum for this guild to ${maximum}.`)
+            .setFooter(version, picture)
+            .setTimestamp()
+            .setColor("#00FF00")
             message.channel.send(SuccessEmbed);
         }
     },
