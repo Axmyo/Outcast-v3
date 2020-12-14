6e112c8c26be2562a4ce0314bb1f78732fa92501
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-14 14:46:06 -0500

    Backend Update
    - Version bump
    - Major efficiency overhauls
    Commit 2 of 2

--- commands/666.js
+++ commands/666.js
@@ -1,10 +1,10 @@
-const { MessageEmbed } = require('discord.js');
-const { picture } = require('../OutcastAssets/config.json');
+const { MessageEmbed } = require("discord.js");
+const { picture } = require("../OutcastAssets/config.json");
 module.exports = {
-    name: '666',
-    description: 'Learn what commands I have to offer!',
+    name: "666",
+    description: "666",
     cooldown: 5,
-    aliases: ['wewilltakeover'],
+    aliases: ["wewilltakeover"],
     execute(message, args){
         const 666Embed = new MessageEmbed()
             .setTitle("666")
@@ -12,8 +12,8 @@ module.exports = {
             .addField("666", "666")
             .addField("666", "666")
             .addField("666", "666")
-            .setColor("#ff0000")
-            .setFooter("666", `${picture}`)
+            .setColor("#FF0000")
+            .setFooter("666", picture)
             .setTimestamp()
         message.channel.send(666Embed);
     }
