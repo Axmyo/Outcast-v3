25390beae2546fe195f4dc1fbb32b31d226d555d
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-15 08:23:00 -0500

    Update specs.js

--- commands/specs.js
+++ commands/specs.js
@@ -1,14 +1,14 @@
 const { Client, MessageEmbed } = require("discord.js");
-const { version, picture, LastUpdated, changelog, vtype } = require("../OutcastAssets/config.json");
 const { execSync } = require("child_process");
 const si = require("systeminformation");
+const { version, picture, LastUpdated, changelog, vtype } = require("../OutcastAssets/config.json");
 const hostname = execSync ('hostname').toString();
 const client = new Client();
 module.exports = {
 	name: "specs",
 	description: "Look at info on me.",
 	cooldown: 5,
-	aliases: ['botinfo', 'bi'],
+	aliases: ["botinfo", "bi"],
 	async execute(message, args){
 		let totalSeconds = (message.client.uptime / 1000);
 		const days = Math.floor(totalSeconds / 86400);
@@ -30,18 +30,18 @@ module.exports = {
 		const SpecsEmbed = new MessageEmbed()
 		.setTitle("Bot Info")
 		.setDescription(`**Changelog:** \n \`\`${changelog}\`\``)
-		.addField("Version:", `${version}`, true)
-		.addField("Version Type:", `${vtype}`, true)
+		.addField("Version:", version, true)
+		.addField("Version Type:", vtype, true)
 		.addField("Library:", "Discord.js v12", true)
-		.addField("Last Updated:", `${LastUpdated}`, true)
+		.addField("Last Updated:", LastUpdated, true)
 		.addField("Developers:", `${message.client.users.cache.get('613527205103927367').tag} and ${message.client.users.cache.get('490178047325110282').tag}`, true)
-		.addField("Current Host Server:", `${hostname}`, true)
-		.addField("Total RAM Availible:", `${memGB}`, true)
+		.addField("Current Host Server:", hostname, true)
+		.addField("Total RAM Availible:", memGB, true)
 		.addField("Usable RAM:", `test`, true)
 		.addField("Total RAM In Use:", ` gb, mb, kb`, true)
-		.addField("Host Server Specs:", `${hostspecs}`, true)
-		.addField("Uptime:", `${uptime} `, true)
-		.addField("Guilds:", `${client.guilds.cache.size}`, true)
+		.addField("Host Server Specs:", hostspecs, true)
+		.addField("Uptime:", uptime, true)
+		.addField("Guilds:", client.guilds.cache.size, true)
 		.setColor("#FF0000")
 		.setFooter(version, picture)
 		.setTimestamp()
