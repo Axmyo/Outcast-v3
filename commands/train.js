c61da90f31f0f79ac061f7db673267d7199a231b
Ash Fogelson <neonmadnessyt@gmail.com>
2020-12-15 08:27:09 -0500

    Update train.js

--- commands/train.js
+++ commands/train.js
@@ -3,10 +3,10 @@ const fetch = require("node-fetch");
 const{ version, picture } = require("../OutcastAssets/config.json");
 module.exports = {
   name:  "train",
-  description: "Look at random train pics",
+  description: "Look at random train pics.",
   cooldown: 5,
-  aliases: ['trainpic'],
-  execute(message, args){
+  aliases: ["trainpic"],
+  execute(message){
       const link = "https://api.outcastcorp.us/api/";
       fetch(link)
         .then(res => res.json())
