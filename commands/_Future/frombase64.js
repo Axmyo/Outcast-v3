eb8efdd0ee885c29e015cc5e9ab260cafca51b6b
Ash Fogelson <devxml@protonmail.com>
2020-12-16 14:46:54 -0500

    Update frombase64.js

--- commands/_Future/frombase64.js
+++ commands/_Future/frombase64.js
@@ -1,2 +1,25 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed } = require('discord.js');
 const fetch = require("node-fetch");
+const { version, picture } = require("../OutcastAssets/config.json");
+module.exports = {
+  name:  "frombase64",
+  description: "Decode from Base64, courtesy of SomeRandomAPI.",
+  cooldown: 5,
+  execute(message, args){
+    const input = args.join(' ');
+    const link = "https://some-random-api.ml/base64?decode=aGVsbG8=" + input;
+    fetch(link)
+      .then(res => res.json())
+      .then(body => {
+          const Embed = new MessageEmbed()
+          .setAuthor(message.author.tag, message.author.displayAvatarURL())
+    	      .setColor("#000000")
+     	      .setTitle("Decoded From Base64: ")
+      	  .addField("Input:", input)  
+          .addField("Output:", body.text)
+      	    .setFooter(version, picture)
+      	    .setTimestamp()
+   	      message.channel.send(Embed);
+    })
+  },
+};
