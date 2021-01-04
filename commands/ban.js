555eff7479736834a99280ef7ad6426cec42e9eb
Ash Fogelson <devxml@protonmail.com>
2021-01-04 09:08:35 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,6 +3,7 @@ const { version, picture } = require("../OutcastAssets/config.json");
 module.exports = {
 	name: "ban",
 	description: "Ban's a member",
+	guildonly: true,
 	cooldown: 0,
 	aliases: ["boot"],
 	execute(message, args) {
@@ -10,59 +11,57 @@ module.exports = {
 
 		const PermissionEmbed = new MessageEmbed()
 			.setTitle("Error: Missing Permisssions")
-			.addField("Error Code:", '403: Forbidden')
-			.addField("Missing Permission:", '\`\`BAN_MEMBERS\`\`, \`\`ADMINISTRATOR\`\`, or Server Owner')
+			.addFields({name: "Error Code:", value: "403: Forbidden"}, {name: "Missing Permission:", value: "\`\`BAN_MEMBERS\`\`, \`\`ADMINISTRATOR\`\`, or Server Owner"})
 			.setFooter(version, picture)
 			.setTimestamp()
 			.setColor("#FF0000")
 		if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(PermissionEmbed);
-
-		const noMentionedUser = new MessageEmbed()
-			.setTitle("Error: No User Mentioned")
-			.addField("Error Code:", "1000: No User Mentioned")
-			.addField("Error Description:", "You didn\'t mention a user to kick.")
-			.setFooter(version, picture)
-			.setTimestamp()
-			.setColor("#FF0000")
-
-		if (!member) return message.channel.send(noMentionedUser);
+		else if(message.member.hasPermission("BAN_MEMBERS")) {
+			const noMentionedUser = new MessageEmbed()
+				.setTitle("Error: No User Mentioned")
+				.addFields({name: "Error Code:", value: "1000: No User Mentioned"}, {name: "Error Description:", value: "You didn\'t mention a user to kick."})
+				.setFooter(version, picture)
+				.setTimestamp()
+				.setColor("#FF0000")
+			if (!member) return message.channel.send(noMentionedUser);
 
 
 
-		const cannotBanUser = new MessageEmbed()
-			.setTitle('Error: Cannot Ban User')
-			.addField('Error Code:', '999')
-			.addField('Error Description:', '')
-			.setFooter(version, picture)
-			.setTimestamp()
-			.setColor("#FF0000")
-		if (!member.bannable) return message.channel.send(cannotBanUser);
+			const cannotBanUser = new MessageEmbed()
+				.setAuthor(message.author.tag, message.author.displayAvatarURL())
+				.setTitle("Error: Cannot Ban User")
+				.addField({name: "Error Code:", value: "999"}, {name: "Error Description:", value: "User either has \`\`ADMINISTRATOR\`\` or is a higher role than you are."})
+				.setFooter(version, picture)
+				.setTimestamp()
+				.setColor("#FF0000")
+			if (!member.bannable) return message.channel.send(cannotBanUser);
 
-		let reason = args.slice(1).join(" ");
-		if (!reason) reason = "No Reason Provided.";
-		const bannedEmbed = new MessageEmbed()
-			.setTitle("Outcast Moderation")
-			.setDescription(`You have been banned from ${message.guild.name}.`)
-			.addField("Reason:", reason1)
-			.addField("Moderator:", message.author.tag)
-			.setFooter(version, picture)
-			.setTimestamp()
-			.setColor("#FF0000")
-	
-		const successEmbed = new MessageEmbed()
-			.setTitle("Success!")
-			.setDescription(`Successfully banned ${member}: ${reason}.`)
-			.setFooter(version, picture)
-			.setTimestamp()
-			.setColor("#00FF00")        
-		return member.send(bannedEmbed)
-      		.then(() => {
-        		member.ban({reason: `${message.author.tag}:${reason}`});
-        		message.channel.send(successEmbed);
-		})
-      		.catch(() => {
-        		member.ban({reason: `${message.author.tag}:${reason}`});
-			message.channel.send(successEmbed);
-    		});
+			let reason = args.slice(1).join(" ");
+			if (!reason) reason = "No Reason Provided.";
+			const bannedEmbed = new MessageEmbed()
+				.setTitle("Outcast Moderation")
+				.setDescription(`You have been banned from ${message.guild.name}.`)
+				.addFields({name: "Reason:", value: reason1}, {name: "Moderator:", value: message.author.tag})
+				.setFooter(version, picture)
+				.setTimestamp()
+				.setColor("#FF0000")
+		
+			const successEmbed = new MessageEmbed()
+				.setAuthor(message.author.tag, message.author.displayAvatarURL())	
+				.setTitle("Success!")
+				.setDescription(`Successfully banned ${member}: ${reason}.`)
+				.setFooter(version, picture)
+				.setTimestamp()
+				.setColor("#00FF00")        
+			return member.send(bannedEmbed)
+				.then(() => {
+					member.ban({reason: `${message.author.tag}:${reason}`});
+					message.channel.send(successEmbed);
+			})
+				.catch(() => {
+					member.ban({reason: `${message.author.tag}:${reason}`});
+				message.channel.send(successEmbed);
+			});
+		}
 	}
 };
