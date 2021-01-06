10cbb146564fe7cdacf6909e2d7f8236fe9cb2e6
Ash Fogelson <devxml@protonmail.com>
2021-01-06 08:46:47 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,18 +1,16 @@
-const { Client, MessageEmbed } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 const { version, picture } = require("../OutcastAssets/config.json");
 module.exports = { 
-	name: "invite",
-	description: "Invite Outcast to your server.",
-	cooldown: 5,
-	execute(message, args){
-		const InviteEmbed = new MessageEmbed()
-			.setTitle("Invite Outcast To Your Server")
-			.addField("Stable Release:", "https://outcastcorp.us/outcast/invite")
-			.addField("Alpha Release", "Soon:tm:")
-			.addField("Support Server:", "https://outcastcorp.us/discord")
-			.setColor("#FF0000")
-			.setFooter(version, picture)
-			.setTimestamp()
-		message.channel.send(InviteEmbed);
-	}
+    name: "invite",
+    description: "Invite Outcast to your server.",
+    cooldown: 5,
+    execute(message){
+        const InviteEmbed = new MessageEmbed()
+        .setTitle("Invite Outcast To Your Server")
+        .addFields({name: "Stable Release:", value: "https://outcastcorp.us/outcast/invite"}, {name: "Alpha Release:", value: "Soon:tm:"}, {name: "Support Server:", value: "https://outcastcorp.us/discord"})
+        .setColor("#FF0000")
+        .setFooter(version, picture)
+        .setTimestamp()
+        message.channel.send(InviteEmbed);
+    }
 };
